b06143f87fe459d336d503bdc09e76c614d71e16
AmitSheth9 <amit.sheth9@gmail.com>
2022-02-08 13:48:45 -0600

    wrote post route, User model insert function, UserService create function and test, with test passing when new user is created and added to db

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,22 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
+
+const mockUser = {
+  email: 'mock',
+  password: 'pass',
+};
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+
+  const agent = request.agent(app);
+
+  const user = await UserService.create({ ...mockUser, ...userProps });
+};
+
+
 
 describe('backend routes', () => {
   beforeEach(() => {
@@ -10,5 +26,15 @@ describe('backend routes', () => {
 
   afterAll(() => {
     pool.end();
+  });
+
+  it('creates a new user', async () => {
+    const res = await request(app).post('/api/v1/users').send(mockUser);
+    const { email } = mockUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      email
+    });
   });
 });
